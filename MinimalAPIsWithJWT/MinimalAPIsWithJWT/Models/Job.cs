c817e76a6815593819f31f27e5655ea81ed86cb9
Nagarjunareddy Sannapareddy <Nagarjunareddy.Sannapareddy@inmar.com>
2022-05-02 21:57:43 +0530

    first commit

---
+++ MinimalAPIsWithJWT/MinimalAPIsWithJWT/Models/Job.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace MinimalAPIsWithJWT.Models
+{
+    public class Job
+    {
+        public Guid JobId { get; set; }
+        public int JobStateId { get; set; }
+        public string JobMessage { get; set; }
+        // public string Jobtype { get; set; }
+        // public string JobSubtype { get; set; }
+        // public DateTime LastupdatedUTC { get; set; }
+        // public int OracleJobProcessId { get; set; }
+    }
+
+    public class JobDB : DbContext
+    {
+        public JobDB(DbContextOptions<JobDB> options): base(options){}
+        public DbSet<Job> Job => Set<Job>();
+    }
+}
---
+++ MinimalAPIsWithJWT/MinimalAPIsWithJWT/Program.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using MinimalAPIsWithJWT.Models;
+using  Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<JobDB>(options =>{
+    options.UseSqlServer("Server=nwswsqlfincp001.inmar.com,61001;Database=FinanceApi;Trusted_Connection=false;TrustServerCertificate=True;MultipleActiveResultSets=true;Application Name=Finance API;Integrated Security=false;User Id=svcFinanceApi;Password=Wf$m49vctXE3A@K#s*2Z"); 
+});
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<TokenService>(new TokenService());
+builder.Services.AddSingleton<IUserRepositoryService>(new UserRepositoryService());
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+{
+    opt.TokenValidationParameters = new()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+builder.Services.AddSwaggerGen(c =>
+{
+    var securityScheme = new OpenApiSecurityScheme
+    {
+        Name = "JWT Authentication",
+        Description = "Enter JWT Bearer token **_only_**",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer", // must be lower case
+        BearerFormat = "JWT",
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+    c.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {securityScheme, new string[] { }}
+    });
+});
+var app = builder.Build();
+app.Urls.Add("http://localhost:3000");
+//app.MapGet("/", () => "Hello World!");
+app.MapGet("/jobs", async (int pageNumber, int pageSize, JobDB db) =>
+    // await db.Job.Skip((pageNumber - 1) * pageSize)
+    // .Take(pageSize).ToListAsync()
+{
+    var userDto =new UserRepositoryService().GetUser(new UserModel(){Password = "abc123", UserName = "admin"}); 
+    if (userDto == null)
+    {
+        //response.StatusCode = 401;
+        return;
+    }
+
+    if (builder != null)
+    {
+        if (builder.Configuration != null)
+        {
+            var token =new TokenService().BuildToken(builder.Configuration["Jwt:Key"], builder.Configuration["Jwt:Issuer"],builder.Configuration["Jwt:Audience"], userDto);
+           // response.WriteAsJsonAsync(new { token = token });
+        // return await token.ToCharArray();
+
+        }
+    }
+}
+   
+    
+    
+);
+
+
+// app.MapPost("/login", () =>
+// {
+//     return postUser([FromBodyAttribute]
+//     UserModel userModel, TokenService tokenService,
+//     IUserRepositoryService userRepositoryService, HttpResponse response);
+// });
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseAuthentication();
+app.UseAuthorization();
+app.Run();
+
+ void postUser([FromBodyAttribute] UserModel userModel, TokenService tokenService,
+    IUserRepositoryService userRepositoryService, HttpResponse response)
+{
+    var userDto = userRepositoryService.GetUser(userModel);
+    if (userDto == null)
+    {
+        response.StatusCode = 401;
+        return;
+    }
+
+    if (builder != null)
+    {
+        if (builder.Configuration != null)
+        {
+            var token = tokenService.BuildToken(builder.Configuration["Jwt:Key"], builder.Configuration["Jwt:Issuer"],builder.Configuration["Jwt:Audience"], userDto);
+            response.WriteAsJsonAsync(new { token = token });
+        }
+    }
+
+    return;
+    
+}
